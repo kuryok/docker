b0c2ef5e5cb0d74f16a39ebeee4254001fb0b88f
kuryok <38151211+kuryok@users.noreply.github.com>
2025-01-07 02:53:27 -0300

    OCS-Inventory

---
+++ 2.8.1/conf/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+$cfg['Servers'][1]['host'] = 'localhost';
+$cfg['Servers'][1]['user'] = 'root';
+$cfg['Servers'][1]['password'] = 'password';
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['compress'] = false;
+$cfg['Servers'][1]['AllowNoPassword'] = false;
+?>
